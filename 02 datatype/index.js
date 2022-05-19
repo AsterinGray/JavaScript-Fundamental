0e71ce5360318d3a79acb7d0737de5d7dfb47434
AsterinGray <raymondtiandinatan@gmail.com>
2022-05-19 22:30:54 +0700

    add data type

---
+++ 02 datatype/index.js
@@ -0,0 +1,35 @@
+/**
+ * Data Types
+ * - String
+ * - Number
+ * - Boolean
+ * - Object
+ * - Undefined
+ */
+
+// String
+let string1 = "";
+let string2 = "";
+let string3 = ``;
+
+// Number
+let number1 = 1;
+let number2 = 10e1;
+let number3 = 1.5;
+
+// Boolean
+let boolean1 = true;
+let boolean2 = false;
+
+// Object
+let object1 = {};
+let object2 = [];
+
+// Function
+function function1() {}
+let function2 = function () {};
+let function3 = () => {};
+
+// Others
+let nullValue = null;
+let undefinedValue;
